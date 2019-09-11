a9be37c44d850e460af40a84c51bb91b70f155b2
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-09-12 00:19:16 +0200

    Removed public dir

--- public/component---src-pages-about-js-05cb7d3fe871a4293a3c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{207:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(58),c=a(204),r=a(205);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-05cb7d3fe871a4293a3c.js.map
