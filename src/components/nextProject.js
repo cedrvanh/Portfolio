e68231e014ea6b9271fffb3ab57a771883258c3f
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-09-12 16:28:28 +0200

    Fixed Next Project btn

--- src/components/nextProject.js
+++ src/components/nextProject.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const NextProject = ({ next }) => (
-    <Link to={ next } className="bar-link">
+    <Link to={ next.slug } className="bar-link">
+        { console.log(next) }
         <section className="bar flex-center">
             <div className="container--wide" style={{ zIndex: '999'}}>
                 <h2 className="project-detail__next">Next Project</h2>
