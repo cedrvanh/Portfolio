e033ba820559a766bb08fa45ff08776af9b9d599
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-09-14 20:06:41 +0200

    Changed Next Project btn

--- src/components/nextProject.js
+++ src/components/nextProject.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-const NextProject = ({ next }) => (
-    <Link to={ next.slug } className="bar-link">
-        <section className="bar flex-center">
-            <div className="container--wide" style={{ zIndex: '999'}}>
-                <h2 className="project-detail__next">Next Project</h2>
-            </div>
-        </section>  
-    </Link>
-)
+const NextProject = ({ next }) => {
+    const reverseSlug = (slug) => {
+        const words = slug.split('-');
+
+        for(let i = 0; i < words.length; i++) {
+            const word = words[i];
+            words[i] = word.charAt(0).toUpperCase() + word.slice(1);
+        }
+    
+        return words.join(' ');
+    }
+
+    return (
+        <Link to={ next.slug } className="bar-link">
+            <section className="bar flex-center">
+                <div className="container--wide" style={{ zIndex: '999'}}>
+                    <h2 className="project-detail__next text-medium">Next Project</h2>
+                    <h1 className="project-detail__next-title">{ reverseSlug(next.slug) }</h1>
+                </div>
+            </section>  
+        </Link>
+    )
+}
 
 export default NextProject;
