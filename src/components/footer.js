22b4173ec3c408fa2c56f85d6854dbc0e2e3ef8d
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-09-14 16:33:23 +0200

    Added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="container--wide">
-                <p>@Cedric Vanhaegenberg</p>
+                <h1>Do you have any questions? <br></br> Let's talk! <a href="mailto:c.vanhaegenberg@outlook.com" className="contact__mail">c.vanhaegenberg@outlook.com</a></h1>
             </div>
         </footer>
     )
