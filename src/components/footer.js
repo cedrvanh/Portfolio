ac4960efb3479aab1e2646ebf4191853ff12a3c7
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-10-17 14:29:23 +0200

    Updated links

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,15 +7,15 @@ const Footer = () => {
                 <div className="footer-wrapper">
                     <div className="row">
                         <div className="col-6">
-                            <h2>Do you have any questions? <br></br> Let's talk! <a href="mailto:c.vanhaegenberg@outlook.com" className="footer__mail">cedric@vanhaegenberg.be</a></h2>
+                            <h2>Do you have any questions? <br></br> Let's talk! <a href="mailto:c.vanhaegenberg@outlook.com" className="footer__mail">c.vanhaegenberg@outlook.com</a></h2>
                             <div className="footer__social">
-                                <a href="#" className="footer__social--link">LinkedIn</a>
+                                <a href="https://linkedin.com/in/cedric-vanhaegenberg-4421ba195/" target="_blank" className="footer__social--link">LinkedIn</a>
                                 <a href="https://github.com/cedrvanh" target="_blank" className="footer__social--link">Github</a>
                             </div>
                         </div>
                         <div className="col-6 text-right footer-right">
                             <h2 className="resume">Check out my resume</h2>
-                            <a href="#" className="btn btn--primary">View Resume</a>
+                            <a href="https://drive.google.com/file/d/1NgkzSLlyp-tG3AM-hyqpKdVyL0Fkck1r/view?usp=sharing" target="_blank" className="btn btn--primary">View Resume</a>
                         </div>
                     </div>
                     <div className="footer__bottom">
