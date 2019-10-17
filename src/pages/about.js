b3d8cd67b30b51114725164e9ef323928e7099f9
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-10-17 14:33:32 +0200

    Changed text

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,7 +14,7 @@ const AboutPage = () => (
         <h2>I'm a Full Stack Developer from Kortrijk, Belgium.</h2>
         <p className="highlight-text"><span className="highlight">Hello! I'm Cedric Vanhaegenberg and I'm a student currently in my final year of the Bachelor Graphic and Digital Media in the Artevelde University College Ghent.</span></p> 
         <p>
-        My first experience with coding was when I was 13 years old and I've been addicted ever since. 
+        My first experience with coding was when I was 15 years old and I've been addicted ever since. 
         I'm a passionate developer who absolutely loves discovering and developing new solutions to your problems.
         Writing clean and efficient code is something I really enjoy doing and when I'm working on a project I give it my all and try to make it a meaningful learning experience. 
         I deliver the full creative process from user cases and wireframes to a working prototype. I consider myself a little bit of a designer as well. I enjoy creating good looking user interfaces with a good user experience.
